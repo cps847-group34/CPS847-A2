13d1985836365cbeed61d5781aadcad761d9dd9c
Rylan <36867509+rfabico@users.noreply.github.com>
2021-04-08 17:35:26 -0400

    Create unittest_multilist.py

---
+++ unittest_multilist.py
@@ -0,0 +1,20 @@
+import unittest
+import multilist as multi
+
+
+class TestMultiplicationOperation(unittest.TestCase):
+
+    def test_empty(self):
+        self.assertEqual(multi.multiply_list([]), 0)
+
+    def test_zero(self):
+        self.assertEqual(multi.multiply_list([0, 1, 2, 55, 100]), 0)
+
+    def test_one(self):
+        self.assertEqual(multi.multiply_list([55]), 55)
+
+    def test_multiple(self):
+        self.assertEqual(multi.multiply_list([3, 4, 10]), 120)
+
+    def test_multiple2(self):
+        self.assertEqual(multi.multiply_list([1, 2, 3, 5, 10]), 300)
