00c2580940ea8efb5104cfa3689b3734979dabc4
alter-ego-vic <57118202+alter-ego-vic@users.noreply.github.com>
2021-04-11 18:08:18 -0400

    Publish to GitHub

--- multilist.py
+++
@@ -1,8 +0,0 @@
-def multiply_list(value_list):
-    total = 1
-    if len(value_list) == 0:
-        total = 0
-    for i in value_list:
-        if isinstance(i, int):
-            total = total * i
-    return total
