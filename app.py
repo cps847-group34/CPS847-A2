6c761847c1ecc3e109741d4b5febe5529520df2a
alter-ego-vic <57118202+alter-ego-vic@users.noreply.github.com>
2021-04-11 23:30:00 -0400

    another try with new files

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('Hello World!')
 
 if __name__ == "__main__":
     # Only for debugging while developing
