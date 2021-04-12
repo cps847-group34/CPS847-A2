c7a17052e412d08bb7289c56638525807c89f217
matcha927 <72629469+matcha927@users.noreply.github.com>
2021-04-11 22:13:54 -0400

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,15 @@
+import json
+
+def lambda_handler(event, context):
+  first_name = event.get('first_name', None)
+  last_name = event.get('last_name', None)
+
+  if first_name is None: 
+    raise Exception('Please provide a first name.')
+  if last_name is None: 
+    raise Exception('Please provide a last name.')
+
+  return {
+    'statusCode': 200,
+    'output': first_name + " " + last_name
+  }
